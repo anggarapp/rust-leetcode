1e91c46ab3de600f08bfec9a15d35766ee506336
Anggara Permana Putra <anggaraperpe@gmail.com>
2025-05-10 15:14:00 +0700

    chore: divide test case each 20 problems

---
+++ tests/p21_to_p40.rs
@@ -0,0 +1,90 @@
+use rust_solve_leetcode::solver::*;
+
+#[test]
+fn test_p21_merge_two_lists() {
+    let node_one = Some(Box::new(ListNode { val: 4, next: None }));
+    let node_one = Some(Box::new(ListNode {
+        val: 2,
+        next: node_one,
+    }));
+    let node_one = Some(Box::new(ListNode {
+        val: 1,
+        next: node_one,
+    }));
+
+    let node_two = Some(Box::new(ListNode { val: 4, next: None }));
+    let node_two = Some(Box::new(ListNode {
+        val: 3,
+        next: node_two,
+    }));
+    let node_two = Some(Box::new(ListNode {
+        val: 1,
+        next: node_two,
+    }));
+
+    let node_result = Some(Box::new(ListNode { val: 4, next: None }));
+    let node_result = Some(Box::new(ListNode {
+        val: 4,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 3,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 2,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 1,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 1,
+        next: node_result,
+    }));
+    assert_eq!(P21::solve_v1(node_one, node_two), node_result);
+
+    let node_one = Some(Box::new(ListNode { val: 4, next: None }));
+    let node_one = Some(Box::new(ListNode {
+        val: 2,
+        next: node_one,
+    }));
+    let node_one = Some(Box::new(ListNode {
+        val: 1,
+        next: node_one,
+    }));
+
+    let node_two = Some(Box::new(ListNode { val: 4, next: None }));
+    let node_two = Some(Box::new(ListNode {
+        val: 3,
+        next: node_two,
+    }));
+    let node_two = Some(Box::new(ListNode {
+        val: 1,
+        next: node_two,
+    }));
+
+    let node_result = Some(Box::new(ListNode { val: 4, next: None }));
+    let node_result = Some(Box::new(ListNode {
+        val: 4,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 3,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 2,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 1,
+        next: node_result,
+    }));
+    let node_result = Some(Box::new(ListNode {
+        val: 1,
+        next: node_result,
+    }));
+    assert_eq!(P21::solve_v2(node_one, node_two), node_result);
+}
